3f71a572a21f05c21f097a28f1587add40858a04
Jordan Gonzalez <jordan.gonzalez@streamlinx.com>
2014-10-15 15:51:31 -0400

    Adding function to dump the tables

--- dumpMetaData.sh
+++ dumpMetaData.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+dump_table() {
+    printf "Dumping $1..."
+    mysqldump -u root -p$DBPASS --extended-insert=FALSE --skip-comments smarts_too_dallas_07142014 $1 > $1.sql
+    printf " OK\n"
+}
 stty -echo
 printf "Password: "
 read DBPASS
@@ -7,88 +12,44 @@ printf " OK\n"
 mysql -u root -p$DBPASS -e exit 2>/dev/null 
 if [ "$?" = "0" ]
 then
-    # domain_meta_data
     # Dependencies: none
-    printf "Dumping domain_meta_data..."
-    mysqldump -u root -p$DBPASS --extended-insert=FALSE --skip-comments smarts_too_dallas_07142014 domain_meta_data > domain_meta_data.sql
-    printf " OK\n"
-    #
+    dump_table domain_meta_data
 
-    # property_meta_data
     # Dependencies: domain_meta_data
-    printf "Dumping property_meta_data..."
-    mysqldump -u root -p$DBPASS --extended-insert=FALSE --skip-comments smarts_too_dallas_07142014 property_meta_data > property_meta_data.sql
-    printf " OK\n"
-    #
+    dump_table property_meta_data
 
-    # area_meta_data
     # Dependencies: domain_meta_data
-    printf "Dumping area_meta_data..."
-    mysqldump -u root -p$DBPASS --extended-insert=FALSE --skip-comments smarts_too_dallas_07142014 area_meta_data > area_meta_data.sql
-    printf " OK\n"
+    dump_table area_meta_data
 
-    # area_query_source
     # Dependencies: domain_meta_data, area_meta_data
-    printf "Dumping area_query_source..."
-    mysqldump -u root -p$DBPASS --extended-insert=FALSE --skip-comments smarts_too_dallas_07142014 area_query_source > area_query_source.sql
-    printf " OK\n"
-    #
+    dump_table area_query_source
 
-    # area_category
     # Dependencies: 
-    printf "Dumping area_category..."
-    mysqldump -u root -p$DBPASS --extended-insert=FALSE --skip-comments smarts_too_dallas_07142014 area_category > area_category.sql
-    printf " OK\n"
-    # 
+    dump_table area_category
 
-    # groups
     # Dependencies: 
-    printf "Dumping groups..."
-    mysqldump -u root -p$DBPASS --extended-insert=FALSE --skip-comments smarts_too_dallas_07142014 groups > groups.sql
-    printf " OK\n"
-    #
+    dump_table groups
 
-    # groups_area
     # Dependencies: 
-    printf "Dumping groups_area..."
-    mysqldump -u root -p$DBPASS --extended-insert=FALSE --skip-comments smarts_too_dallas_07142014 groups_area > groups_area.sql
-    printf " OK\n"
+    dump_table groups_area
 
-    # groups_area_list_view
     # Dependencies: 
-    printf "Dumping groups_area_list_view..."
-    mysqldump -u root -p$DBPASS --extended-insert=FALSE --skip-comments smarts_too_dallas_07142014 groups_area_list_view > groups_area_list_view.sql
-    printf " OK\n"
+    dump_table groups_area_list_view
 
-    # list_view
     # Dependencies: 
-    printf "Dumping list_view..."
-    mysqldump -u root -p$DBPASS --extended-insert=FALSE --skip-comments smarts_too_dallas_07142014 list_view > list_view.sql
-    printf " OK\n"
+    dump_table list_view
 
-    # list_view_field
     # Dependencies: 
-    printf "Dumping list_view_field..."
-    mysqldump -u root -p$DBPASS --extended-insert=FALSE --skip-comments smarts_too_dallas_07142014 list_view_field > list_view_field.sql
-    printf " OK\n"
+    dump_table list_view_field
 
-    # work_flow
     # Dependencies: 
-    printf "Dumping work_flow..."
-    mysqldump -u root -p$DBPASS --extended-insert=FALSE --skip-comments smarts_too_dallas_07142014 work_flow > work_flow.sql
-    printf " OK\n"
+    dump_table work_flow
 
-    # step_definition
     # Dependencies: 
-    printf "Dumping step_definition..."
-    mysqldump -u root -p$DBPASS --extended-insert=FALSE --skip-comments smarts_too_dallas_07142014 step_definition > step_definition.sql
-    printf " OK\n"
+    dump_table step_definition
 
-    # transition_definition
     # Dependencies: 
-    printf "Dumping transition_definition..."
-    mysqldump -u root -p$DBPASS --extended-insert=FALSE --skip-comments smarts_too_dallas_07142014 transition_definition > transition_definition.sql
-    printf " OK\n"
+    dump_table transition_definition
 else
     echo "Couldn't connect..."
 fi
